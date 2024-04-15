3f2d35b38c41c99349e552928d094ab39af8e16a
Swoo <mitchellbraaten@gmail.com>
2024-04-14 23:15:49 -0700

    Add files via upload

---
+++ build/Desktop_Qt_6_6_3_MSVC2019_64bit-Debug/XBox360App_autogen/include/ui_mainwindow.h
@@ -0,0 +1,134 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.6.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QVBoxLayout *verticalLayout;
+    QGroupBox *groupBox;
+    QGridLayout *gridLayout;
+    QLabel *label;
+    QLineEdit *ipLineEdit;
+    QLabel *label_2;
+    QLineEdit *usernameLineEdit;
+    QLabel *label_3;
+    QLineEdit *passwordLineEdit;
+    QPushButton *saveSettingsButton;
+    QPushButton *connectButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(400, 300);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        verticalLayout = new QVBoxLayout(centralwidget);
+        verticalLayout->setObjectName("verticalLayout");
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName("groupBox");
+        gridLayout = new QGridLayout(groupBox);
+        gridLayout->setObjectName("gridLayout");
+        label = new QLabel(groupBox);
+        label->setObjectName("label");
+
+        gridLayout->addWidget(label, 0, 0, 1, 1);
+
+        ipLineEdit = new QLineEdit(groupBox);
+        ipLineEdit->setObjectName("ipLineEdit");
+
+        gridLayout->addWidget(ipLineEdit, 0, 1, 1, 1);
+
+        label_2 = new QLabel(groupBox);
+        label_2->setObjectName("label_2");
+
+        gridLayout->addWidget(label_2, 1, 0, 1, 1);
+
+        usernameLineEdit = new QLineEdit(groupBox);
+        usernameLineEdit->setObjectName("usernameLineEdit");
+
+        gridLayout->addWidget(usernameLineEdit, 1, 1, 1, 1);
+
+        label_3 = new QLabel(groupBox);
+        label_3->setObjectName("label_3");
+
+        gridLayout->addWidget(label_3, 2, 0, 1, 1);
+
+        passwordLineEdit = new QLineEdit(groupBox);
+        passwordLineEdit->setObjectName("passwordLineEdit");
+
+        gridLayout->addWidget(passwordLineEdit, 2, 1, 1, 1);
+
+
+        verticalLayout->addWidget(groupBox);
+
+        saveSettingsButton = new QPushButton(centralwidget);
+        saveSettingsButton->setObjectName("saveSettingsButton");
+
+        verticalLayout->addWidget(saveSettingsButton);
+
+        connectButton = new QPushButton(centralwidget);
+        connectButton->setObjectName("connectButton");
+
+        verticalLayout->addWidget(connectButton);
+
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 400, 21));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "XBox 360 App (ALPHA)", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "Xbox 360 Settings", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "IP:", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "Username:", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Password:", nullptr));
+        saveSettingsButton->setText(QCoreApplication::translate("MainWindow", "Save Settings", nullptr));
+        connectButton->setText(QCoreApplication::translate("MainWindow", "Connect", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
